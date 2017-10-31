4152c3c976a5ca63bc3bf7f1e1df682a8d74d031
Kaveesha Ilukkumbure <15032197+kvsh443@users.noreply.github.com>
2017-11-01 03:28:59 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -48,7 +48,7 @@ def user_leave_greet(message):
 		except:
 			l_name=" "
 		title = message.chat.title
-		leftmember=str(f_name+l_name)
+		leftmember=str(f_name+" "+l_name)
 		bot.send_message(message.chat.id, "_"+leftmember+"_ 'left' *"+title+"* `see you soon`",parse_mode='Markdown')
 	else:
 		title = message.chat.title
